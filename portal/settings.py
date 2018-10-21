449188857c240d9cb976f4dfc2c1d027238ba985
Guilherme Luis <guile.hm@hotmail.com>
2018-10-21 17:03:22 -0300

    Setup heroku

--- portal/settings.py
+++ portal/settings.py
@@ -129,3 +129,17 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'register.User'
+
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    ALLOWED_HOSTS = ['gui-portal.herokuapp.com']
+    DEBUG = False
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )

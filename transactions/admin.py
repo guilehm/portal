5e9854cf1633449abc7c2ed8b26733022ec7379c
Guilherme Luis <guile.hm@hotmail.com>
2018-10-20 22:14:45 -0300

    Create service order admin

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
-from transactions.models import Event
+from transactions.models import Event, ServiceOrder
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('machine', 'category', 'subject')
     list_filter = ('machine', 'category', 'date_added', 'date_changed')
+
+
+@admin.register(ServiceOrder)
+class ServiceOrderAdmin(admin.ModelAdmin):
+    list_display = ('code', 'event', 'category', 'machine', 'priority')
+    list_filter = ('category', 'priority')
+    search_fields = ('event', 'category', 'subject', 'description')

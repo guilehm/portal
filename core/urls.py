278a552ab0422e5b822771cd6879d7f0704d9cd1
Guilherme Luis <guile.hm@hotmail.com>
2018-10-21 15:42:01 -0300

    Create login url

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ from core import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('accounts/login/', views.login_view, name='login')
 ]

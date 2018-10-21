ca0fc2fa1054abe03a8cdb0c66ab31f6c72dcbb0
Guilherme Luis <guile.hm@hotmail.com>
2018-10-21 15:49:54 -0300

    Create login view and update template

--- core/views.py
+++ core/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+
 
 @login_required
 def index(request):
     return render(request, 'core/index.html')
+
+
+def login_view(request):
+    if request.method != 'POST':
+        form = AuthenticationForm()
+    else:
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            authenticated_user = authenticate(
+                username=request.POST['username'],
+                password=request.POST['password']
+            )
+            login(request, authenticated_user)
+            return redirect('core:index')
+    return render(request, 'core/login.html', {'form': form})

0f7eef4e775546ef16ac7cf37324bab20b9084fb
Guilherme Luis <guile.hm@hotmail.com>
2018-10-22 20:29:10 -0300

    Create logo and mini logo template tags

---
+++ core/templatetags/core_tags.py
@@ -0,0 +1,17 @@
+from django import template
+from register.models import MainCompany
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('core/tags/logo_tag.html')
+def logo_tag():
+    company = MainCompany.objects.first()
+    return {'logo': company.logo}
+
+
+@register.inclusion_tag('core/tags/logo_mini_tag.html')
+def logo_mini_tag():
+    company = MainCompany.objects.first()
+    return {'logo': company.logo_thumb}

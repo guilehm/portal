bf895d3f0f91e6faa2a6e06dcb26da51ca4ccb4a
Guilherme Luis <guile.hm@hotmail.com>
2018-10-23 23:01:00 -0300

    Create template tags

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -1,6 +1,7 @@
 from django import template
 
 from register.models import MainCompany
+from utils.weather import icon_dict
 
 register = template.Library()
 
@@ -15,3 +16,13 @@ def logo_tag():
 def logo_mini_tag():
     company = MainCompany.objects.first()
     return {'logo': company.logo_thumb}
+
+
+@register.filter(name='get_icon')
+def get_icon(code):
+    return icon_dict.get(code)
+
+
+@register.filter(name='f_to_c')
+def fahrenheit_to_celsius(temperature_f):
+    return (float(temperature_f) - 32) / 1.8

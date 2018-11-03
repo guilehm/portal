436be54d08575fc51b8f9bd38b9ac1e6f49c253c
Guilherme Luis <guile.hm@hotmail.com>
2018-11-03 20:15:54 -0300

    Create scheduler file

---
+++ transactions/scheduler.py
@@ -0,0 +1,9 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from model_mommy import mommy
+
+scheduler = BackgroundScheduler()
+
+
+@scheduler.scheduled_job('interval', minutes=2)
+def create_message():
+    return mommy.make('register.Message')

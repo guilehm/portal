06bc7cddefa92fa9217e66f3ec7ce857485036cb
Guilherme Luis <guile.hm@hotmail.com>
2018-11-04 20:40:54 -0200

    Change import

--- transactions/scheduler.py
+++ transactions/scheduler.py
@@ -1,12 +1,14 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from model_mommy import mommy
 
+from register.models import Message
+
 scheduler = BackgroundScheduler()
 
 
 @scheduler.scheduled_job('interval', minutes=2)
 def create_message():
-    return mommy.make('register.Message')
+    return mommy.make(Message)
 
 
 scheduler.start()

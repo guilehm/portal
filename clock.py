5e71d2a5ae5a83d2c413b497b528902c381a7123
Guilherme Luis <guile.hm@hotmail.com>
2018-11-05 23:22:39 -0200

    Skip clock from isort and flake8

--- clock.py
+++ clock.py
@@ -1,9 +1,10 @@
-import django
+import django  # noqa
+django.setup()
+
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from register.models import Message
 
-django.setup()
 
 scheduler = BackgroundScheduler()
 

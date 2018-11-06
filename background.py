16ec99361378798f8c9b5123ff4e5404054836c7
Guilherme <guile.hm@hotmail.com>
2018-11-06 00:13:28 -0200

    Remove unused function

--- background.py
+++ background.py
@@ -1,18 +1,15 @@
 import django
 django.setup()
 
-from django.utils import timezone
 from apscheduler.schedulers.background import BackgroundScheduler
-from register.models import Message
 
 
 scheduler = BackgroundScheduler()
 
 
 @scheduler.scheduled_job('interval', minutes=1)
-def create_message():
-    message = Message(message=str(timezone.now()))
-    message.save()
+def do_something():
+    pass  # do something here
 
 
 scheduler.start()
